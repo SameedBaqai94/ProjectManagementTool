a2678408b9eba2d48323017e8fc238990883dbd9
sameedbaqai94 <sameedbaqai94@gmail.com>
2022-03-16 20:42:29 -0400

    config

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL='postgresql://postgres:Shadow_12@127.0.0.1/fastapi'
+dname = os.getenv("database_name", "")
+uname = os.getenv("uname","")
+SQLALCHEMY_DATABASE_URL=f'postgresql://postgres:{uname}@127.0.0.1/{dname}'
 
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
 
